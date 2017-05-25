645a26cac1affddc784bc3356cc51ae080564eb5
Leonardo Pérez <ephelsa@hotmail.com>
2017-05-25 15:29:56 -0500

    Corrección del tabHost.

--- Profesor/src/main/java/co/edu/udea/compumovil/gr09_20171/controlparental/Views/FragmentAsistencia.java
+++ Profesor/src/main/java/co/edu/udea/compumovil/gr09_20171/controlparental/Views/FragmentAsistencia.java
@@ -1,8 +1,59 @@
 package co.edu.udea.compumovil.gr09_20171.controlparental.Views;
 
-/**
- * Created by leonardo on 25/05/17.
- */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
-public class FragmentAsistencia {
+import co.edu.udea.compumovil.gr09_20171.controlparental.R;
+
+public class FragmentAsistencia extends Fragment {
+
+    private String [] estudiantes;
+    private boolean asisChecked;
+    private boolean justiChecked;
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Declaraciones.
+        //View view = inflater.inflate(R.layout.asistencia, container, false);
+
+        return null;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    private String[] llenarEstudiantes (int estudiantes) { // Además recibiría la base de datos
+        String [] listado = new String[estudiantes];
+        String estudiante = "Estudiante"; // Aquí iría el nombre de cada chicuelo.
+
+        for (int i = 1; i <= estudiantes; i++) {
+            listado[i - 1] = estudiante + (Integer) i;  // Aquí tenemos que quitar el i.
+        }
+
+        return listado;
+    }
+
+    public String[] getEstudiantes() {
+        return estudiantes;
+    }
+
+    public boolean isAsisChecked() {
+        return asisChecked;
+    }
+
+    public boolean isJustiChecked() {
+        return justiChecked;
+    }
 }
