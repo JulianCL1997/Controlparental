b151500a638f67c893914c807cf5b787ca1d810d
Julian <juliancl1997@gmail.com>
2017-05-28 16:57:00 -0500

    correcion dialog

--- Profesor/src/main/java/co/edu/udea/compumovil/gr09_20171/controlparental/Dialogs/CambiarNota.java
+++ Profesor/src/main/java/co/edu/udea/compumovil/gr09_20171/controlparental/Dialogs/CambiarNota.java
@@ -1,10 +1,10 @@
 package co.edu.udea.compumovil.gr09_20171.controlparental.Dialogs;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 
 import co.edu.udea.compumovil.gr09_20171.controlparental.R;
