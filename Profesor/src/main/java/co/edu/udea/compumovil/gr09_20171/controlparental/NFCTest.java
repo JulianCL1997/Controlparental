3d890a1245b1dab6c10ecdc0143c3a417333b837
Julian <juliancl1997@gmail.com>
2017-05-29 17:05:58 -0500

    NFC ejemplo

--- Profesor/src/main/java/co/edu/udea/compumovil/gr09_20171/controlparental/NFCTest.java
+++ Profesor/src/main/java/co/edu/udea/compumovil/gr09_20171/controlparental/NFCTest.java
@@ -21,9 +21,7 @@ public class NFCTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfctest);
-        textViewInfo = (TextView) findViewById(R.id.info);
-        textViewTagInfo = (TextView) findViewById(R.id.taginfo);
-        textViewBlock = (TextView) findViewById(R.id.block);
+       textViewBlock = (TextView) findViewById(R.id.block);
 
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (nfcAdapter == null) {
@@ -55,24 +53,7 @@ public class NFCTest extends AppCompatActivity {
             if (tag == null) {
                 textViewInfo.setText("tag == null");
             } else {
-                String tagInfo = tag.toString() + "\n";
 
-                tagInfo += "\nTag Id: \n";
-                byte[] tagId = tag.getId();
-                tagInfo += "length = " + tagId.length + "\n";
-                for (int i = 0; i < tagId.length; i++) {
-                    tagInfo += Integer.toHexString(tagId[i] & 0xFF) + " ";
-                }
-                tagInfo += "\n";
-
-                String[] techList = tag.getTechList();
-                tagInfo += "\nTech List\n";
-                tagInfo += "length = " + techList.length + "\n";
-                for (int i = 0; i < techList.length; i++) {
-                    tagInfo += techList[i] + "\n ";
-                }
-
-                textViewInfo.setText(tagInfo);
                 readMifareClassic(tag);
             }
         } else {
@@ -86,50 +67,7 @@ public class NFCTest extends AppCompatActivity {
     public void readMifareClassic(Tag tag) {
         MifareClassic mifareClassicTag = MifareClassic.get(tag);
 
-        String typeInfoString = "--- MifareClassic tag ---\n";
-        int type = mifareClassicTag.getType();
-        switch (type) {
-            case MifareClassic.TYPE_PLUS:
-                typeInfoString += "MifareClassic.TYPE_PLUS\n";
-                break;
-            case MifareClassic.TYPE_PRO:
-                typeInfoString += "MifareClassic.TYPE_PRO\n";
-                break;
-            case MifareClassic.TYPE_CLASSIC:
-                typeInfoString += "MifareClassic.TYPE_CLASSIC\n";
-                break;
-            case MifareClassic.TYPE_UNKNOWN:
-                typeInfoString += "MifareClassic.TYPE_UNKNOWN\n";
-                break;
-            default:
-                typeInfoString += "unknown...!\n";
-        }
-
-        int size = mifareClassicTag.getSize();
-        switch (size) {
-            case MifareClassic.SIZE_1K:
-                typeInfoString += "MifareClassic.SIZE_1K\n";
-                break;
-            case MifareClassic.SIZE_2K:
-                typeInfoString += "MifareClassic.SIZE_2K\n";
-                break;
-            case MifareClassic.SIZE_4K:
-                typeInfoString += "MifareClassic.SIZE_4K\n";
-                break;
-            case MifareClassic.SIZE_MINI:
-                typeInfoString += "MifareClassic.SIZE_MINI\n";
-                break;
-            default:
-                typeInfoString += "unknown size...!\n";
-        }
-
-        int blockCount = mifareClassicTag.getBlockCount();
-        typeInfoString += "BlockCount \t= " + blockCount + "\n";
-        int sectorCount = mifareClassicTag.getSectorCount();
-        typeInfoString += "SectorCount \t= " + sectorCount + "\n";
-
-        textViewTagInfo.setText(typeInfoString);
-        new ReadMifareClassicTask(mifareClassicTag).execute();
+         new ReadMifareClassicTask(mifareClassicTag).execute();
     }
 
     private class ReadMifareClassicTask extends AsyncTask<Void, Void, Void> {
