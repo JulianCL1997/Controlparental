3248ed584f91d73c8c8e8a1276aca86dd064df20
Julian <juliancl1997@gmail.com>
2017-05-30 13:01:44 -0500

    Profesor 100%

--- Profesor/src/main/java/co/edu/udea/compumovil/gr09_20171/controlparental/MainActivity.java
+++ Profesor/src/main/java/co/edu/udea/compumovil/gr09_20171/controlparental/MainActivity.java
@@ -1,58 +1,71 @@
 package co.edu.udea.compumovil.gr09_20171.controlparental;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
+import android.view.Window;
 
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import co.edu.udea.compumovil.gr09_20171.controlparental.Model.CursoMateria;
-import co.edu.udea.compumovil.gr09_20171.controlparental.Model.Estudiante;
+import co.edu.udea.compumovil.gr09_20171.controlparental.Views.CursoMateriaView;
+import co.edu.udea.compumovil.gr09_20171.controlparental.Views.LoginView;
+import co.edu.udea.compumovil.gr09_20171.controlparental.Views.TabProfesor;
 
 
 public class MainActivity extends AppCompatActivity {
-    List<CursoMateria> cursos = new ArrayList<>();
-    List<Estudiante> estudents = new ArrayList<>();
-    List<String> existe = new ArrayList<String>();
-
-    /*@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference reference = database.getReference("Materias");
-
-        Calendar calendar = Calendar.getInstance();
-        String dia = String.valueOf(calendar.get(calendar.DAY_OF_MONTH));
-        String mes = String.valueOf(calendar.get(calendar.MONTH) + 1);
-        String anio = String.valueOf(calendar.get(calendar.YEAR));
-        String fecha = dia + "-" + mes + "-" + anio;
-
-        //Registro si asistio
-        reference.child("Historia").child("9")
-                .child("a").child("97032709625").child("Asistencias").child(fecha).child("Fecha").setValue(fecha);
-    }*/
-    TextView boton;
+    private static final long LOAD_DATABASE = 100;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler);
-
-        boton = (TextView) findViewById(R.id.soyunboton);
-
-        boton.setOnClickListener(new View.OnClickListener() {
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View v) {
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
             }
-        });
+        };
+
+
+        // Set portair orientacion
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //esconder barra de titulo
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                //iniciar siguiente actividad
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    Intent intent = new Intent(MainActivity.this, CursoMateriaView.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+
+
+                }else{
+                    Intent intent = new Intent(MainActivity.this, LoginView.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+
+
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, LOAD_DATABASE);
     }
 }
